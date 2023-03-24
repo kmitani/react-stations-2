a26278f592b34d51c02087d5199ed67bbd872f4f
kmitani <mk.mitani.keita@gmail.com>
2023-03-24 15:16:53 +0900

    ルーティング

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import * as React from 'react'
 import './App.css';
-import ApiFetch from './components/ApiFetch.js'
+import Top from './components/Top.js'
+import CreateThread from './components/CreateThread.js'
+import ShowComment from './components/ShowComment.js'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import ThreadNew from './components/ThreadNew.js'
 
 
 function App() {
-
   return (
-    <div>
-      <h1>新着スレッド</h1>
-      <ApiFetch />
-      {/* <ThreadNew /> */}
-    </div>
+    <BrowserRouter>
+      <h1>掲示板アプリ</h1>
+      <Switch>
+      <Route exact path="/" component={Top} />
+      <Route path="/create-thread" component={CreateThread} />
+      <Route path="/:id" component={ShowComment} />
+      <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
+}
+function NotFound() {
+  return("ページがありません")
 }
 
 export default App;
